e023ff04478876f1d806d78abac832a36830c132
sDuyToans <duytoannguyenledh4@gmail.com>
2022-08-01 13:26:49 +0700

    made sign in form, styles the authentication page

---
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import "./sign-in-form.styles.scss";
+
+import {
+  createUserDocumentFromAuth,
+  signInWithGooglePopup,
+  signInAuthUserWithEmailAndPassword
+} from "../../utils/firebase/firebase.utils";
+import FormInput from "../form-input/form-input.component";
+import Button from "../button/button.component";
+const defaultFormField = {
+  email: "",
+  password: "",
+};
+
+const SignInForm = () => {
+  const [formFields, setFormFiels] = useState(defaultFormField);
+  const { email, password } = formFields;
+
+  const resetFormFields = () => {
+    setFormFiels(defaultFormField);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await signInAuthUserWithEmailAndPassword(email, password);
+      resetFormFields();
+    } catch (err) {
+      switch(err.code){
+        case 'auth/wrong-password': 
+          alert('incorrect password for email');
+          break;
+        case 'auth/user-not-found':
+          alert('no user associated with this email');
+          break;
+        default:
+          console.log(err)
+      }
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormFiels({ ...formFields, [name]: value });
+  };
+
+  const signInWithGoogle = async () => {
+    const { user } = await signInWithGooglePopup();
+    await createUserDocumentFromAuth(user);
+  };
+  return (
+    <div className="sign-up-container">
+      <h2>Already have an account?</h2>
+      <span>Sign in with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label="Email"
+          type="email"
+          required
+          onChange={handleChange}
+          name="email"
+          value={email}
+        />
+
+        <FormInput
+          label="Password"
+          type="password"
+          required
+          onChange={handleChange}
+          name="password"
+          value={password}
+        />
+        <div className="buttons-container">
+          <Button type="submit">Sign In</Button>
+          <Button type="button" buttonType="google" onClick={signInWithGoogle}>
+            Google Sign In
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignInForm;
--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -2,10 +2,10 @@
 // import { async } from '@firebase/util';
 // import { getRedirectResult } from 'firebase/auth';
 // import { useEffect } from 'react';
+import SignInForm from '../../components/sign-in-form/sign-in-form.component';
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
-import {  createUserDocumentFromAuth, signInWithGooglePopup, signInWithGoogleRedirect } from '../../utils/firebase/firebase.utils';
-
-const SighIn = () =>{
+import './authentication.styles.scss'
+const Authentication = () =>{
     // useEffect( () =>{
     //     async function checkDataWhenSignInWithGoogleRedirect(){
     //         const response = await getRedirectResult(auth);
@@ -16,17 +16,10 @@ const SighIn = () =>{
     //     checkDataWhenSignInWithGoogleRedirect();
     // }, []);
 
-    const logGoogleUser = async () =>{
-        const { user } = await signInWithGooglePopup();
-        const userDocRef = await createUserDocumentFromAuth(user);
-    };
 
     return (
-        <div>
-            <h1>Sign In Page</h1>
-            <button onClick={logGoogleUser}>
-                Sign in with Google Popup
-            </button>
+        <div className='authentication-container'>
+            <SignInForm/>
             <SignUpForm/>
             {/* <button onClick={signInWithGoogleRedirect}>
                 Sign in with Google Redirect
@@ -35,4 +28,4 @@ const SighIn = () =>{
     )
 }
 
-export default SighIn;
+export default Authentication;
