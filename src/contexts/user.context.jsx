f2d2ae0445341f0cca6ff7f08a16a64273852c2a
sDuyToans <duytoannguyenledh4@gmail.com>
2022-08-08 00:23:30 +0700

    change from useState to useReduce in context

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from "../utils/firebase/firebase.utils";
+import { createAction } from "../utils/reducer/reducer.utils";
 
 
 //as the actual value you want to access
@@ -8,10 +9,37 @@ export const UserContext = createContext({
     setCurrentUser: () => null
 });
 
-export const UserProvider = ({ children }) =>{
-    const [currentUser, setCurrentUser]  = useState(null);
-    const value = { currentUser, setCurrentUser };
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER'
+}
 
+const userReducer = (state, action) =>{
+    const { type, payload } = action;
+
+    switch(type){
+        case USER_ACTION_TYPES.SET_CURRENT_USER: 
+            return {
+                ...state,
+                currentUser: payload
+            }
+        default: 
+        throw new Error(`Unhanled type ${type} in userReducer`)
+    }
+}
+
+const INITIAL_STATE = {
+    currentUser: null
+}
+
+export const UserProvider = ({ children }) =>{
+    const [ { currentUser } , dispatch ] = useReducer(userReducer, INITIAL_STATE)
+
+    const setCurrentUser = (user) =>{
+        dispatch(
+            createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user))
+    }
+
+    const value = { currentUser, setCurrentUser };
 
     useEffect(() =>{
         const unsubcribe = onAuthStateChangedListener( async (user) =>{
